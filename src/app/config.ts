2d5354229fecd211838e1406646e2a91c7a0a5b2
Michel Bijnen <michelbijnenmichelbijnen@gmail.com>
2020-10-06 17:24:25 +0200

    fix: dependency updates

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,3 @@
 export class config {
-  //public static apiUrl: string = 'https://michelbijnen.nl:4201';
-  public static apiUrl: string = 'https://192.168.27.156:4201';
+  public static apiUrl: string = 'http://localhost:8080';
 }
